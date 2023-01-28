86c400ebc5c8415e1a1c73edb9c5e7723d94f366
Vladislav Alekseev <vlad.alex14@gmail.com>
2023-01-28 14:12:44 +0500

    add

--- src/test/java/selenide/SelenideSearchTest.java
+++ src/test/java/selenide/SelenideSearchTest.java
@@ -2,8 +2,8 @@ package selenide;
 
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Condition.href;
 import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class SelenideSearchTest {
@@ -12,9 +12,10 @@ public class SelenideSearchTest {
     void checkSelenideSearchJUnit5Test () {
         open("https://github.com/selenide/selenide");
         $("#wiki-tab").click();
-        $("#wiki-body ul").shouldHave(text("Soft assertions"));
-        $$("#wiki-body ul a").findBy(href("/selenide/selenide/wiki/SoftAssertions")).click();
-        $("#wiki-content").shouldHave(text("Using JUnit5"));
+        $(".js-wiki-more-pages-link").click();
+        $("#wiki-pages-box").shouldHave(text("SoftAssertions"));
+        $(".wiki-pages-box").find(byText("SoftAssertions")).click();
+        $("#wiki-body").shouldHave(text("JUnit5"));
         sleep(10000);
     }
 }
